a2538dd295f8829784324e554d1f31188fd7b969
Ramanjaneyulu <ramanjaneyuludvj@gmail.com>
2024-07-13 12:00:50 +0530

    .\index.js

---
+++ JSTASK1/index.js
@@ -0,0 +1,53 @@
+
+// DATA Types
+
+console.log(typeof 1)
+
+console.log(typeof "1")
+
+console.log(typeof true)
+
+const myVar = null;
+
+console.log(myVar); 
+console.log(typeof myVar); 
+
+
+let c;
+console.log(typeof c);
+
+
+let a =1
+let b = 2
+console.log("addition", a+b) 
+
+console.log('Substraction', a-b)
+
+console.log('Multiplication', a*b)
+
+console.log("modulus", a%b)
+
+console.log("divison", a/b)
+
+// console.log(a=b)
+
+console.log("lessthan", a<b)
+
+console.log("greterthan", a>b)
+
+console.log("lessthan equalto", a<=b)
+
+console.log("greterthan equalto", a>=b)
+
+console.log("not equal", a!=b)
+
+console.log("equal", a==b)
+
+console.log("Post increment", a++)
+
+console.log("Post decrement", a--)
+
+console.log("pre increment", ++a)
+
+console.log("pre decrement", --a)
+
